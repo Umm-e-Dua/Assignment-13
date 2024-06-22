6a2449c4df2116e497f3b9782ecaac7119ffa41e
Umm-e-Dua <160483094+Umm-e-Dua@users.noreply.github.com>
2024-06-23 01:34:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+//13 Assignment
+var transportationModes = ["Sports bike", "Bus", "Car", "Sports car"];
+transportationModes.forEach(function (mode) { return console.log("I would like to own a ".concat(mode)); });
---
+++ main.ts
@@ -0,0 +1,3 @@
+//13 Assignment
+let transportationModes = ["Sports bike","Bus","Car","Sports car"];
+transportationModes.forEach(mode => console.log(`I would like to own a ${mode}`));
